be55dcc59f38ac4d73c7a197b2eee2fc3a88c41a
Rémi <r3mi@users.sourceforge.net>
2014-05-07 09:27:27 +0200

    simplify SimplePageClient

--- src/main/java/com/thalesgroup/concurrent_crawler/client/simple/SimplePageClient.java
+++ src/main/java/com/thalesgroup/concurrent_crawler/client/simple/SimplePageClient.java
@@ -7,15 +7,8 @@ import java.net.URL;
 
 public class SimplePageClient extends PageClient {
 
-    private static final int DELAY_MILLIS = 20;
-
     @Override
     public Page getPage(URL url) throws IOException {
-        try {
-            Thread.sleep(DELAY_MILLIS);
-        } catch (InterruptedException ex) {
-            throw new IOException("interrupted", ex);
-        }
-        return new SelfReferencingPage(url);
+    	return new SelfReferencingPage(url);
     }
 }
